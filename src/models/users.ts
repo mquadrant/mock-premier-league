9b606f6dd4c64c9f6c78f840cba9cd4fd566e6f2
Benjamin Mark <mquadrant@gmail.com>
2019-09-16 22:14:24 +0100

    created a user model

---
+++ src/models/users.ts
@@ -0,0 +1,55 @@
+import mongoose, { Schema, Document } from 'mongoose'
+import { ITeam } from './teams'
+
+export interface IUser extends Document {
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+    role: String
+    club?: string | ITeam
+}
+
+const userSchema: Schema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, 'A user must have first name'],
+            trim: true,
+            maxlength: [
+                40,
+                'A user name must have less or equal to 40 characters',
+            ],
+        },
+        lastName: {
+            type: String,
+            required: [true, 'A user must have first name'],
+            trim: true,
+            maxlength: [
+                40,
+                'A user name must have less or equal to 40 characters',
+            ],
+        },
+        email: {
+            type: String,
+            required: [true, 'A user must have email'],
+            trim: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'A user must have password'],
+        },
+        role: {
+            type: String,
+            enum: {
+                values: ['ADMIN', 'USER'],
+                message: 'role is either: ADMIN or USER',
+            },
+            required: [true, 'User should be attached to a role'],
+        },
+    },
+    { timestamps: true }
+)
+
+export default mongoose.model<IUser>('User', userSchema)
