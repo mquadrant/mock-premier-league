2c95a24bbf01e0e962123a67678832f233e0e7f6
Benjamin Mark <mquadrant@gmail.com>
2019-09-16 22:14:34 +0100

    created a team model

---
+++ src/models/teams.ts
@@ -0,0 +1,59 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export interface ITeam extends Document {
+    clubName: string
+    about?: string
+    arena: string
+    owner: string
+    headCoach: string
+    founded: string
+    nickName?: string
+    players: IPlayer[]
+}
+
+export interface IPlayer extends Document {
+    playerName: string
+    position: string
+    shirtNo: number
+}
+
+const teamSchema: Schema = new Schema(
+    {
+        clubName: {
+            type: String,
+            required: true,
+        },
+        about: {
+            type: String,
+        },
+        arena: {
+            type: String,
+            required: true,
+        },
+        owner: {
+            type: String,
+            required: true,
+        },
+        headCoach: {
+            type: String,
+            required: true,
+        },
+        founded: {
+            type: String,
+            required: true,
+        },
+        nickName: {
+            type: String,
+        },
+        players: [
+            {
+                playerName: String,
+                position: String,
+                shirtNo: Number,
+            },
+        ],
+    },
+    { timestamps: true }
+)
+
+export default mongoose.model<ITeam>('Team', teamSchema)
