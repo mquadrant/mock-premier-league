7eebe63d4a7c92e3e5287604d91262b17bcee149
Benjamin Mark <mquadrant@gmail.com>
2019-09-17 23:00:02 +0100

    setup mongoose connection

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,35 @@ import express, { Request, Response, NextFunction } from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import env from './config'
 
 import indexRouter from './routes/index'
 
 const app = express()
-
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+
+//Connection to mongoDB
+const uri = `${env.databaseURL}`
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully!')
+})
+connection.once('open', () => {})
+connection.on('error', () => {
+    console.log('Error Connecting To Database')
+})
 
 //REST route
 app.use('/api/v1', indexRouter)
