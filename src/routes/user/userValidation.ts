7690964a9cb17db7a5bfe2a58b05fb2c819389f6
Benjamin Mark <mquadrant@gmail.com>
2019-09-17 21:37:19 +0100

    created user validation with joi

--- src/routes/user/userValidation.ts
+++ src/routes/user/userValidation.ts
@@ -3,12 +3,16 @@ import Joi from 'joi'
 function validateSignup(createUser: Express.Request) {
     // define joi validation schema for User
     const schema = Joi.object().keys({
-        firstName: Joi.string(),
+        firstName: Joi.string().required(),
         lastName: Joi.string(),
         email: Joi.string()
             .email()
             .required(),
         password: Joi.string(),
+        role: Joi.string(),
+        club: Joi.string(),
     })
     return Joi.validate(createUser, schema)
 }
+
+export { validateSignup }
