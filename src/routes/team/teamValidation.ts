6d054062edf1bc3454ed86142044640f9e5be935
Benjamin Mark <mquadrant@gmail.com>
2019-09-22 16:12:56 +0100

    added team update body validation

--- src/routes/team/teamValidation.ts
+++ src/routes/team/teamValidation.ts
@@ -8,11 +8,24 @@ function validateTeam(createTeam: Express.Request) {
         email: Joi.string()
             .email()
             .required(),
-        password: Joi.string(),
-        role: Joi.string(),
+        password: Joi.string().required(),
+        role: Joi.string().required(),
         club: Joi.string(),
     })
     return Joi.validate(createTeam, schema)
 }
 
-export { validateTeam }
+function validateEditTeam(editTeam: Express.Request) {
+    // define joi validation schema for Team
+    const schema = Joi.object().keys({
+        firstName: Joi.string(),
+        lastName: Joi.string(),
+        email: Joi.string().email(),
+        password: Joi.string(),
+        role: Joi.string(),
+        club: Joi.string(),
+    })
+    return Joi.validate(editTeam, schema)
+}
+
+export { validateTeam, validateEditTeam }
