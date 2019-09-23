c66872724d95f5aef259243aa05ac81bf5795daa
Benjamin Mark <mquadrant@gmail.com>
2019-09-23 15:17:38 +0100

    added authorization middleware to fixture routers

--- src/routes/fixture/fixtures.ts
+++ src/routes/fixture/fixtures.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import adminAccess from './../../middleware/admin-access'
+import checkAuth from './../../middleware/check-auth'
 const router = express.Router()
 import {
     addFixture,
@@ -9,14 +11,14 @@ import {
 } from '../../controllers/fixturesController'
 
 /* POST create fixture. */
-router.post('/', addFixture)
+router.post('/', checkAuth, adminAccess, addFixture)
 /* GET fixture . */
-router.get('/', getAllFixtures)
+router.get('/', checkAuth, getAllFixtures)
 /* GET single fixture. */
-router.get('/:fixtureId', removeFixture)
+router.get('/:fixtureId', checkAuth, removeFixture)
 /* PATCH fixture updating. */
-router.patch('/:fixtureId', editFixture)
+router.patch('/:fixtureId', checkAuth, adminAccess, editFixture)
 /* DELETE deleting a fixture by id. */
-router.delete('/:fixtureId', viewSingleFixture)
+router.delete('/:fixtureId', checkAuth, adminAccess, viewSingleFixture)
 
 export default router
