6257258993904b3ec38c6a6ec6d7fba7b72f8139
Benjamin Mark <mquadrant@gmail.com>
2019-09-23 13:11:31 +0100

    set unifiedTopology to true in mongoose connection

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,7 @@ mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
 })
 const connection = mongoose.connection
 connection.once('open', () => {
