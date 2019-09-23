9b82d589563b2de7a668f41da80ce11e019274ac
Benjamin Mark <mquadrant@gmail.com>
2019-09-23 13:12:11 +0100

    connected all the feature routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import usersRouter from './user/users'
+import authRouter from './auth/auth'
+import teamRouter from './team/teams'
+import fixtureRouter from './fixture/fixtures'
 const router = express.Router()
 
 /* GET home page. */
@@ -9,5 +12,14 @@ router.get('/', function(_req, res, _next) {
 
 /* USER Routes. */
 router.use('/users', usersRouter)
+
+/* USER Auth Routes. */
+router.use('/auth', authRouter)
+
+/* TEAM Routes. */
+router.use('/teams', teamRouter)
+
+/* FIXTURE Routes. */
+router.use('/fixtures', fixtureRouter)
 
 export default router
