eb59e014cc06e2c5370ef2e2c1da21031fda0182
Benjamin Mark <mquadrant@gmail.com>
2019-09-24 01:19:01 +0100

    made some team validation required

--- src/routes/fixture/fixtureValidation.ts
+++ src/routes/fixture/fixtureValidation.ts
@@ -3,11 +3,11 @@ import Joi from 'Joi'
 function validateFixture(editFixture: Express.Request) {
     // define joi validation schema for Team
     const schema = Joi.object().keys({
-        homeTeam: Joi.string(),
-        awayTeam: Joi.string(),
+        homeTeam: Joi.string().required(),
+        awayTeam: Joi.string().required(),
         homeGoals: Joi.number(),
         awayGoals: Joi.number(),
-        matchTime: Joi.string(),
+        matchTime: Joi.string().required(),
         stadium: Joi.string(),
         referee: Joi.string(),
         isPlayed: Joi.boolean(),
